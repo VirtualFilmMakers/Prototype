220185d8da5d0186deb32f2959ebe7ceacdb926e
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-08-09 23:07:54 +0900

    UI_Thumbnail Update

---
+++ Source/VFM_proto/Private/D_CameraBase.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "D_CameraBase.h"
+
+// Sets default values
+AD_CameraBase::AD_CameraBase()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AD_CameraBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AD_CameraBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AD_CameraBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
