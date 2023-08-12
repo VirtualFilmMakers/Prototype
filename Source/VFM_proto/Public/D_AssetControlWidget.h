3371de9e3accdb74263ccf1ca999b32403b37621
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-08-12 18:48:51 +0900

    DragDrop Function / Ehanced Input for AssetController / LightController

---
+++ Source/VFM_proto/Public/D_AssetControlWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "D_AssetControlWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VFM_PROTO_API UD_AssetControlWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
