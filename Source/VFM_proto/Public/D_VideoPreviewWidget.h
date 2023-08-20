819b8bc8ec58fa521ae70b78d01309446af89fee
obdo <81908835+fortress43-dev@users.noreply.github.com>
2023-08-20 15:15:43 +0900

    Create VideoEditor / And Drag Frop Fuctions

---
+++ Source/VFM_proto/Public/D_VideoPreviewWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "D_VideoPreviewWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VFM_PROTO_API UD_VideoPreviewWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
--- Source/VFM_proto/VFM_proto.Build.cs
+++ Source/VFM_proto/VFM_proto.Build.cs
@@ -8,7 +8,7 @@ public class VFM_proto : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" ,"Json" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
